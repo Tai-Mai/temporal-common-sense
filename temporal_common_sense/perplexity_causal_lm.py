10bfa81d8d424d7c8e0505a18dc975d750712eb8
Tai Mai <contact@taimai.de>
2024-07-22 21:12:37 +0200

    Add claude and gpt4o data

--- temporal_common_sense/perplexity_causal_lm.py
+++ temporal_common_sense/perplexity_causal_lm.py
@@ -1,11 +1,11 @@
 import evaluate
 
-model = 'gpt2'
+model = "gpt2"
 
 perplexity = evaluate.load("perplexity", module_type="metric")
 input_texts = ["lorem ipsum", "Happy Birthday!"]
 
-results = perplexity.compute(model_id=model,
-                             add_start_token=True,
-                             predictions=input_texts)
+results = perplexity.compute(
+    model_id=model, add_start_token=True, predictions=input_texts
+)
 print(results)
