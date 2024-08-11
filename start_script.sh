42214ea610e7f5256c0eda21fec8914dd87a5dfa
tai@vm <contact@taimai.de>
2024-08-11 18:43:37 +0000

    Add llama 3.1 normalized

--- start_script.sh
+++ start_script.sh
@@ -1,4 +1,4 @@
-docker run -d --name tcs --rm -it -v "$PWD:/app" --env-file .env --runtime nvidia tcs python -u measure_common_sense.py --lm_mode causal --model_id meta-llama/Meta-Llama-3.1-8B #--quantization
+docker run -d --name tcs --rm -it -v "$PWD:/app" --env-file .env --runtime nvidia tcs python -u measure_common_sense.py --lm_mode causal --model_id meta-llama/Meta-Llama-3.1-8B --normalize #--quantization
 #docker run -d --name tcs --rm -it -v "$PWD:/app" --env-file .env --runtime nvidia tcs python -u measure_common_sense.py --lm_mode masked --model_id roberta-base --normalize
 #docker run -d --name tcs --rm -it -v "$PWD:/app" --env-file .env --runtime nvidia tcs python -u measure_common_sense.py --lm_mode causal --model_id gpt2 --normalize
 OUTPUT_FILE=output.txt
